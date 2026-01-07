0b42ee45cc7a9b0f39d1381fbc934ec664b5f4b8
Salt Chang <me@saltchang.com>
2026-01-07 14:59:42 +0800

    feat: update kitty config and nvim config for macos

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -130,11 +130,39 @@ return {
         "mg979/vim-visual-multi",
         branch = "master",
         init = function()
+            local is_mac = vim.fn.has("macunix") == 1
             vim.g.VM_default_mappings = 0
-            vim.g.VM_maps = {
-                ["Add Cursor Down"] = "<C-S-j>",
-                ["Add Cursor Up"] = "<C-S-k>",
+            if is_mac then
+                vim.g.VM_maps = {
+                    ["Add Cursor Down"] = "<D-S-j>",
+                    ["Add Cursor Up"] = "<D-S-k>",
+                    ["Find Under"] = "<D-d>",
+                }
+            else
+                vim.g.VM_maps = {
+                    ["Add Cursor Down"] = "<C-S-j>",
+                    ["Add Cursor Up"] = "<C-S-k>",
+                }
+            end
+        end,
+    },
+
+    {
+        "hrsh7th/nvim-cmp",
+        opts = function()
+            local cmp = require("cmp")
+            local conf = require("nvchad.configs.cmp")
+
+            local mymappings = {
+                ["<Up>"] = cmp.mapping.select_prev_item(),
+                ["<Down>"] = cmp.mapping.select_next_item(),
+                ["<Tab>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                }),
             }
+            conf.mapping = vim.tbl_deep_extend("force", conf.mapping, mymappings)
+            return conf
         end,
     },
 }
