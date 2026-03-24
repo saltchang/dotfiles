3a16b560c23b7b6a027db737119efaf264ca84f9
Salt Chang <me@saltchang.com>
2026-03-24 22:56:53 +0800

    feat: add formatter configs

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -3,10 +3,21 @@ return {
         "stevearc/conform.nvim",
         opts = function(_, opts)
             opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
-                json = { "biome" },
-                jsonc = { "biome" },
+                json = { "biome", "prettier", stop_after_first = true },
+                jsonc = { "biome", "prettier", stop_after_first = true },
+                typ = { "biome" },
                 sh = { "shfmt" },
                 zsh = { "beautysh" },
+                javascript = { "biome", "prettier", stop_after_first = true },
+                javascriptreact = { "biome", "prettier", stop_after_first = true },
+                typescript = { "biome", "prettier", stop_after_first = true },
+                typescriptreact = { "biome", "prettier", stop_after_first = true },
+            })
+
+            opts.formatters = vim.tbl_deep_extend("force", opts.formatters or {}, {
+                biome = {
+                    require_cwd = true, -- 只有在專案根目錄找到 biome.json(c) 時才執行 biome
+                },
             })
         end,
     },
