5495a23403a10516bf6650739c644ab9a5a3b86a
Salt Chang <me@saltchang.com>
2026-04-07 23:19:47 +0800

    feat: add styles for diagnostic dialog

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,14 @@ return {
                     -- don't show the inline text for HINT levels
                     severity = { min = vim.diagnostic.severity.INFO },
                 },
+                underline = {
+                    -- don't show the undercurl text for HINT levels
+                    severity = { min = vim.diagnostic.severity.INFO },
+                },
+                float = {
+                    border = "rounded", -- available styles: "single", "double", "rounded", "solid", "shadow"
+                    source = "always",
+                },
             },
             servers = {
                 pyright = {
