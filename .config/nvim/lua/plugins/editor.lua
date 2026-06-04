e63116ab30b8e30540e3e47323b5a60b56059dcd
Salt Chang <me@saltchang.com>
2026-06-04 23:05:13 +0800

    feat: change tab to apply completion

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -301,22 +301,16 @@ return {
     },
 
     {
-        "hrsh7th/nvim-cmp",
-        opts = function()
-            local cmp = require("cmp")
-            local conf = require("nvchad.configs.cmp")
-
-            local mymappings = {
-                ["<Up>"] = cmp.mapping.select_prev_item(),
-                ["<Down>"] = cmp.mapping.select_next_item(),
-                ["<Tab>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }),
-            }
-            conf.mapping = vim.tbl_deep_extend("force", conf.mapping, mymappings)
-            return conf
-        end,
+        "saghen/blink.cmp",
+        opts = {
+            -- Use <Tab> to accept the completion instead of <CR>.
+            -- super-tab: <Tab> selects+accepts the (first) item, jumps
+            -- snippets, or falls back to a normal tab when the menu is closed.
+            -- <S-Tab> jumps snippets backward; <Up>/<Down> select items.
+            keymap = {
+                preset = "super-tab",
+            },
+        },
     },
 
     {
